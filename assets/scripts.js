29bd75c9fb1b8ec11f833b1f359ccb51446b2615
iulian-redinciuc <iredinciuc.com>
2018-03-13 00:24:12 +0200

    added menu and scroll to section

--- assets/scripts.js
+++ assets/scripts.js
@@ -18,3 +18,40 @@ $(document).ready(function(){
     loop: true
   });
 });
+
+$('#main_section .hero_item').click(function(){
+  var clickedHero = $(this).attr("scroll-attr");
+
+  $('html, body').animate({
+      scrollTop: $("#" + clickedHero).offset().top
+  }, 1000);
+  return false;
+});
+
+$(document).ready(function(){
+	$('#nav-icon4').click(function(){
+		$(this).toggleClass('open');
+	});
+});
+
+$(document).ready(function(){
+  $('#nav-icon4').click(function(){
+    if($(this).hasClass('open')) {
+      $(".menu_overlay").fadeIn(500)
+    } else {
+      $(".menu_overlay").fadeOut(500)
+    }
+  })
+
+})
+
+$('.menu_overlay nav ul li a').click(function(){
+
+  var clickedLink = $(this).attr('href');
+  $(".menu_overlay").fadeOut(500)
+  $('#nav-icon4').removeClass('open');
+  $('html, body').animate({
+      scrollTop: $(clickedLink).offset().top
+  }, 1000);
+  return false;
+});
